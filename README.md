ba64f95a7dbf907042e61835badb02207af5a848
AvaAvarai <alice.williams.tech@gmail.com>
2023-08-22 08:51:35 -0700

    Initial simulation

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# BoidsNim
-A simple 2D boids flocking simulation implemented using Nim, OpenGL, and GLUT.
+# Boids Simulation in Nim
+
+A simple boids flocking simulation implemented using Nim, OpenGL, and GLUT.
+
+![Example Program Behavior](ex1.png)
+
+## Features
+
+- Simulates the flocking behavior of boids using the classic three rules: alignment, cohesion, and separation.
+- Boids avoid walls when they get close to the screen boundaries.
+- Each boid is rendered in a unique shade of blue for easier distinction.
+
+## Installation and Setup
+
+1. Ensure you have Nim, OpenGL, and GLUT installed on your machine.
+2. Clone the repository:
+
+```bash
+git clone <repository-url>
+```
+
+Navigate to the project directory:
+
+```bash
+cd <repository-directory>
+```
+
+Compile and run the project:
+
+```bash
+nim c -r boids.nim
+```
+
+## Usage
+
+Watch the boids flock and interact in real-time.
+Adjust parameters like the number of boids, maximum speed, view radius, etc., in the source code to see how it affects the simulation.
+
+## Dependencies and Required Libraries on Windows
+
+To run the boids simulation on Windows, you'll primarily need the FreeGLUT library:
+
+### FreeGLUT (The Free OpenGL Utility Toolkit)
+
+FreeGLUT is an open-source alternative to the original GLUT library. It provides a portable API for handling window creation, input events, and other tasks useful for OpenGL-based applications.
+
+**Required DLL**: `freeglut.dll` (64-bit) or `freeglut32.dll` (32-bit).
+
+### Installation Instructions
+
+1. Ensure you have FreeGLUT installed on your system.
+2. Place the `freeglut.dll` file (if not already present) in the same directory as your executable or in a directory listed in your system's `PATH` environment variable.
+
+## License
+
+This project is licensed under the GPL3 License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- Craig Reynolds for the original boids algorithm.
+- OpenAI for guidance and suggestions in refining the simulation.
---
+++ boids.nim
@@ -0,0 +1,190 @@
+import opengl/glut, opengl, opengl/glu, math, random
+
+const
+    BOID_COUNT = 500
+    VIEW_RADIUS = 20.0
+    MAX_SPEED = 2.0
+    MAX_FORCE = 0.05
+    WINDOW_WIDTH = 900.0
+    WINDOW_HEIGHT = 600.0
+    ALIGNMENT_WEIGHT = 1.2
+    COHESION_WEIGHT = 1.0
+    SEPARATION_WEIGHT = 1.5
+    WALL_MARGIN = 10.0
+
+type
+  Vector = tuple[x, y: float32]
+  Boid = object
+    position: Vector
+    velocity: Vector
+    acceleration: Vector
+    color: float32  # A value between 0.0 (darkest) and 1.0 (brightest)
+
+var boids: array[BOID_COUNT, Boid]
+
+# Utility functions for vector operations
+proc add(a, b: Vector): Vector = (x: a.x + b.x, y: a.y + b.y)
+proc sub(a, b: Vector): Vector = (x: a.x - b.x, y: a.y - b.y)
+proc mult(a: Vector, scalar: float32): Vector = (x: a.x * scalar, y: a.y * scalar)
+proc normalize(a: Vector): Vector =
+  let mag = float32(sqrt(a.x * a.x + a.y * a.y))
+  if mag > 0:
+    return (x: a.x / mag, y: a.y / mag)
+  return a
+
+# Implement the three rules for boid movement
+proc align(boid: Boid): Vector =
+  var sum: Vector = (x: 0.0f, y: 0.0f)
+  var count = 0
+  for other in boids:
+    let d = sqrt((boid.position.x - other.position.x)^2 + (boid.position.y - other.position.y)^2)
+    if d > 0 and d < VIEW_RADIUS:
+      sum = sum.add(other.velocity)
+      count += 1
+  if count > 0:
+    sum = sum.mult(1.0 / count.float32)
+    sum = sum.normalize().mult(MAX_SPEED)
+    return sum.sub(boid.velocity)
+  return (x: 0.0, y: 0.0)
+
+proc cohesion(boid: Boid): Vector =
+  var sum: Vector = (x: 0.0f, y: 0.0f)
+  var count = 0
+  for other in boids:
+    let d = sqrt((boid.position.x - other.position.x)^2 + (boid.position.y - other.position.y)^2)
+    if d > 0 and d < VIEW_RADIUS:
+      sum = sum.add(other.position)
+      count += 1
+  if count > 0:
+    sum = sum.mult(1.0 / count.float32)
+    return sum.sub(boid.position).normalize().mult(MAX_SPEED).sub(boid.velocity)
+  return (x: 0.0, y: 0.0)
+
+proc separation(boid: Boid): Vector =
+  var steer: Vector = (x: 0.0f, y: 0.0f)
+  var count = 0
+  for other in boids:
+    let d = sqrt((boid.position.x - other.position.x)^2 + (boid.position.y - other.position.y)^2)
+    if d > 0 and d < VIEW_RADIUS:
+      let diff = boid.position.sub(other.position).normalize().mult(1.0 / d)
+      steer = steer.add(diff)
+      count += 1
+  if count > 0:
+    steer = steer.mult(1.0 / count.float32)
+  if steer.x != 0 or steer.y != 0:
+    steer = steer.normalize().mult(MAX_SPEED).sub(boid.velocity)
+  return steer
+
+proc avoidWalls(boid: Boid): Vector =
+  var steer: Vector = (x: 0.0f, y: 0.0f)
+
+  # Left Wall
+  if boid.position.x < WALL_MARGIN:
+    steer.x = MAX_SPEED
+  # Right Wall
+  if boid.position.x > WINDOW_WIDTH - WALL_MARGIN:
+    steer.x = -MAX_SPEED
+  # Top Wall
+  if boid.position.y < WALL_MARGIN:
+    steer.y = MAX_SPEED
+  # Bottom Wall
+  if boid.position.y > WINDOW_HEIGHT - WALL_MARGIN:
+    steer.y = -MAX_SPEED
+
+  return steer
+
+proc getOrientation(velocity: Vector): float32 =
+  return arctan2(velocity.y, velocity.x)
+
+# Update the boids' movement
+proc updateBoid(boid: var Boid) =
+  let a = align(boid).normalize().mult(MAX_FORCE)
+  let c = cohesion(boid).normalize().mult(MAX_FORCE)
+  let s = separation(boid).normalize().mult(MAX_FORCE)
+  let w = avoidWalls(boid).normalize().mult(MAX_FORCE)
+    
+  boid.acceleration = boid.acceleration.add(w)
+  boid.acceleration = boid.acceleration.add(a.mult(ALIGNMENT_WEIGHT))
+  boid.acceleration = boid.acceleration.add(c.mult(COHESION_WEIGHT))
+  boid.acceleration = boid.acceleration.add(s.mult(SEPARATION_WEIGHT))
+  boid.velocity = boid.velocity.add(boid.acceleration)
+  if sqrt(boid.velocity.x^2 + boid.velocity.y^2) > MAX_SPEED:
+    boid.velocity = boid.velocity.normalize().mult(MAX_SPEED)
+  boid.position = boid.position.add(boid.velocity)
+  boid.acceleration = (x: 0.0, y: 0.0)
+
+# Initialize boids
+for i in 0..<BOID_COUNT:
+  boids[i] = Boid(
+      position: (
+          x: (rand(WINDOW_WIDTH).float32 + WALL_MARGIN.float32),
+          y: (rand(WINDOW_HEIGHT).float32 + WALL_MARGIN.float32)
+      ), 
+      velocity: (x: (rand(1.0) - 0.5).float32, y: (rand(1.0) - 0.5).float32), 
+      acceleration: (x: 0.0, y: 0.0),
+      color: rand(1.0).float32  # Assign a random shade of blue
+  )
+
+# Update display function
+proc display() {.cdecl.} =
+  glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+  for boid in boids:
+    let angle = getOrientation(boid.velocity) * (180.0 / PI)  # Convert radian to degree
+    glPushMatrix()
+    glTranslatef(boid.position.x, boid.position.y, 0.0)
+    glRotatef(angle, 0.0, 0.0, 1.0)  # Rotate around the Z-axis
+    glBegin(GL_TRIANGLES)
+    glColor3f(0.0, 0.0, boid.color)  
+    glVertex2f(-2.5, -2.5)
+    glVertex2f(2.5, -2.5)
+    glVertex2f(0.0, 2.5)
+    glEnd()
+    glPopMatrix()
+  # render walls
+  glColor3f(1.0, 0.0, 0.0) # Red color for walls
+  glBegin(GL_LINE_LOOP)
+  glVertex2f(WALL_MARGIN, WALL_MARGIN)
+  glVertex2f(WINDOW_WIDTH - WALL_MARGIN, WALL_MARGIN)
+  glVertex2f(WINDOW_WIDTH - WALL_MARGIN, WINDOW_HEIGHT - WALL_MARGIN)
+  glVertex2f(WALL_MARGIN, WINDOW_HEIGHT - WALL_MARGIN)
+  glEnd()
+  glutSwapBuffers()
+
+# Add an idle function to update boids
+proc idle() {.cdecl.} = 
+  for boid in boids.mitems():
+    updateBoid(boid)
+  glutPostRedisplay()
+
+
+proc reshape(width: GLsizei, height: GLsizei) {.cdecl.} =
+  # Compute aspect ratio of the new window
+  if height == 0:
+    return
+
+  # Set the viewport to cover the new window
+  glViewport(0, 0, width, height)
+  glMatrixMode(GL_PROJECTION)
+  glLoadIdentity()
+  gluOrtho2D(0.0, WINDOW_WIDTH, 0.0, WINDOW_HEIGHT) 
+
+var argc: cint = 0
+glutInit(addr argc, nil)
+glutInitDisplayMode(GLUT_DOUBLE)
+glutInitWindowSize(int(WINDOW_WIDTH), int(WINDOW_HEIGHT))
+discard glutCreateWindow("Boids in Nim with OpenGL and GLUT demo")
+
+glutDisplayFunc(display)
+glutIdleFunc(idle)
+glutReshapeFunc(reshape)
+
+loadExtensions()
+
+glClearColor(0.0, 0.0, 0.0, 1.0)                   # Set background color to black and opaque
+glClearDepth(1.0)                                 # Set background depth to farthest
+#glEnable(GL_DEPTH_TEST)                           # Enable depth testing for z-culling
+glDepthFunc(GL_LEQUAL)                            # Set the type of depth-test
+glShadeModel(GL_SMOOTH)                           # Enable smooth shading
+glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST) # Nice perspective corrections
+
+glutMainLoop()
